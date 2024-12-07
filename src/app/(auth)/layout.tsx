e3a4ccaf309b773221454d0c255f0daa055e7c17
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-12-07 17:54:00 -0600

    Fix build errors

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -34,18 +34,7 @@ export default function DashboardLayout({
                     <hr className="flex-grow border-gray-600" />
                 </div>
 
-                <div className="w-full flex flex-col justify-center items-center space-y-6">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        className="bg-white text-[#1C1C1C] border py-2 px-4 w-full max-w-md rounded-xl text-sm hover:scale-105 duration-300 focus:outline-none focus:ring-2 focus:ring-[#1C1C1C] shadow-md"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="bg-white text-[#1C1C1C] border py-2 px-4 w-full max-w-md rounded-xl text-sm hover:scale-105 duration-300 focus:outline-none focus:ring-2 focus:ring-[#1C1C1C] shadow-md"
-                    />
-                </div>
+                { children }
 
                 <button className="bg-white border py-2 w-full max-w-md rounded-xl mt-5 flex justify-center items-center text-sm hover:bg-gray-200 hover:scale-105 duration-300 text-[#1C1C1C] shadow-md">
                     Continue
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,16 @@
 const Signup: React.FC = () => {
     return (
-        <div className="bg-red-500">
-            
+        <div className="w-full flex flex-col justify-center items-center space-y-6">
+            <input
+                type="email"
+                placeholder="Email"
+                className="bg-white text-[#1C1C1C] border py-2 px-4 w-full max-w-md rounded-xl text-sm hover:scale-105 duration-300 focus:outline-none focus:ring-2 focus:ring-[#1C1C1C] shadow-md"
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                className="bg-white text-[#1C1C1C] border py-2 px-4 w-full max-w-md rounded-xl text-sm hover:scale-105 duration-300 focus:outline-none focus:ring-2 focus:ring-[#1C1C1C] shadow-md"
+            />
         </div>
     )
 }
