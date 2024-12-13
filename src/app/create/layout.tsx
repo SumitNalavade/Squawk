c83edd8f84b6c87e5acb211245720e36339361dd
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-12-13 14:18:04 -0600

    Link create sidebar

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
 
                         {/* Right Item */}
                         <div className="flex space-x-8 items-center text-sm">
-                            <a href="#" className="hover:text-gray-600 font-normal">
+                            <Link href={"/create"} className="hover:text-gray-600 font-normal">
                                 My Projects
-                            </a>
+                            </Link>
 
                             <Link href={"/create/new/sources"} className="bg-[#2c2c2c] text-white px-6 py-2 rounded-md">
                                 New Project
--- src/app/create/new/layout.tsx
+++ src/app/create/new/layout.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from 'next/navigation'
 import { FiBook, FiShare2 } from "react-icons/fi";
 import { RiSpeakAiLine } from "react-icons/ri";
 
@@ -8,27 +12,30 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const pathname = usePathname();
+
     return (
         <div className="flex flex-col w-full">
             <main className="flex-grow flex">
                 <div className="h-full space-y-4">
-                    <div className="p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer">
+                    <Link href={"/create/new/sources"} className={`p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer ${pathname === "/create/new/sources" ? "text-blue-500" : ""}`}>
                         <FiBook className="text-3xl" />
 
                         <p>Sources</p>
-                    </div>
+                    </Link>
 
-                    <div className="p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer">
+                    <Link href={"/create/new/tone"} className={`p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer ${pathname === "/create/new/tone" ? "text-blue-500" : ""}`}>
                         <RiSpeakAiLine className="text-3xl" />
 
                         <p>Tone</p>
-                    </div>
+                    </Link>
 
-                    <div className="p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer">
+                    <Link href={"/create/new/share"} className={`p-4 flex flex-col gap-y-1 items-center justify-center hover:bg-gray-200 hover:cursor-pointer ${pathname === "/create/new/share" ? "text-blue-500" : ""}`}>
                         <FiShare2 className="text-3xl" />
 
                         <p>Share</p>
-                    </div>
+                    </Link>
+
                 </div>
 
                 <section className="w-full">
